9bf3d9ac81cb32cde786592d4661f720a59af979
Arpit Kaithwar <arpitkaithwar02@gmail.com>
2022-08-08 02:22:22 +0530

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Textform from './components/Textform';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
+  const [Mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+
+  const toggleMode = () => {
+    if(Mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = '#282c30d6';
+      document.body.style.color = 'white';
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+      <Navbar mode={Mode} toggleMode={toggleMode} />
+      <div className="container my-3">
+        <Routes>
+          <Route exact path="/about" element={<About />}>  
+          </Route>
+          <Route exact path="/" element={<Textform heading="Enter the text to Analyze"/>}>    
+          </Route>
+        </Routes>
+      </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Textform.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+
+export default function Textform(props) {
+    const UpClick = () =>{
+        // console.log("UpperCase was Clicked " + text)
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+    const lwClick = () =>{
+        // console.log("UpperCase was Clicked " + text)
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+    const Clear = () =>{
+        setText('')
+    }
+    const handleExtraSpaces = () =>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+
+    const handleCopy = () =>{
+        var text = document.getElementById("mybox1");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    const handleOnChange = (event) =>{
+        // console.log("On change");
+        setText(event.target.value)
+    }
+
+
+    const [text, setText] = useState('')
+  return (
+    <>
+    <div className="container my-2">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+            <label htmlFor="mybox" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="mybox" placeholder="name@example.com"/>
+        </div>
+        <div className="mb-3">
+            <label htmlFor="mybox" className="form-label">Enter your text</label>
+            <textarea className="form-control" id="mybox1" value={text} onChange={handleOnChange} rows="8" placeholder='Enter your text'></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={UpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={lwClick}>Convert to LowerCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy</button>
+        <button className="btn btn-primary mx-2" onClick={Clear}>Clear</button>
+        <br/>
+        <button className="btn btn-primary m-2" onClick={handleExtraSpaces}>Remove Extra spaces</button>
+    </div>
+    <div className="container my-3">
+        <h2>Your text Summary:</h2>
+        <p>{text.split(" ").length -1} words and {text.length} characters</p>
+        <p>{0.008 * (text.split(" ").length-1)} Minutes take to read </p>
+        <div className='border border-secondary p-4 rounded'>
+            <h2>Preview</h2>
+            <p>{text.length>0?text:"Enter something in above box to preview it."}</p>
+        </div>
+    </div>
+    </>
+    
+  )
+}
