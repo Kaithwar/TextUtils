619f5095bb2ec0dccf7c212368ffd565f0beb996
Arpit Kaithwar <arpitkaithwar02@gmail.com>
2022-08-08 02:31:39 +0530

    Updates

---
+++ precache-manifest.526f0e5b2161922409556e59834dcc1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "356b0e38daeec5e47c09",
+    "url": "/TextUtils/static/css/main.3d38c198.chunk.css"
+  },
+  {
+    "revision": "356b0e38daeec5e47c09",
+    "url": "/TextUtils/static/js/main.356b0e38.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "3e43faa5be22a212c57c",
+    "url": "/TextUtils/static/js/2.3e43faa5.chunk.js"
+  },
+  {
+    "revision": "94fa866583836ad85178",
+    "url": "/TextUtils/static/js/runtime~main.94fa8665.js"
+  },
+  {
+    "revision": "5b6bd950b73f1f0b9fd4a9456f44988c",
+    "url": "/TextUtils/index.html"
+  }
+];
